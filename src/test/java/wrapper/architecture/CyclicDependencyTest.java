fef2c1caa8a8c953598c5f0a804756b89d503d5d
antoine <antoine.perraudat@planimize.com>
2025-12-27 12:18:55 +0100

    renamed test

--- src/test/java/wrapper/architecture/CyclicDependencyTest.java
+++ src/test/java/wrapper/architecture/CyclicDependencyTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 class CyclicDependencyTest {
 
     @Test
-    void packagesMustBeFreeOfCycles() {
+    void wrapperPackageMustBeFreeOfCycles() {
         final JavaClasses importedClasses = new ClassFileImporter()
                 .withImportOption(new ImportOption.DoNotIncludeTests())
                 .importPackages(List.of("..wrapper.."));
