f0dc766d3cfc75260f1904d4037765913b9c77d0
Antoine Perraudat <34171425+AntoinePerraudat92@users.noreply.github.com>
2026-02-22 18:55:44 +0100

    Rename InitialSolution to Hint (#25)

--- src/test/java/wrapper/model/ModelHintTest.java
+++ src/test/java/wrapper/model/ModelHintTest.java
@@ -8,47 +8,46 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.*;
 import static wrapper.util.Constants.EPSILON;
 
-class ModelWarmStartTest {
+class ModelHintTest {
 
     static {
         System.loadLibrary("highs");
         System.loadLibrary("highswrap");
     }
 
-
     @Test
     void parseMustThrowForUnknownVariables() {
         final Model model = new Model();
-        final InitialSolution initialSolution = InitialSolution.of(Map.of(new Variable(32), -45D));
+        final Hint hint = Hint.of(Map.of(new Variable(32), -45D));
 
-        final VariableException exception = assertThrows(VariableException.class, () -> model.parseInitialSolution(initialSolution));
+        final VariableException exception = assertThrows(VariableException.class, () -> model.parseHint(hint));
         assertEquals("Variable with index 32 does not exist in the model", exception.getMessage());
     }
 
     @Test
-    void parseInitialSolutionMustReturnFalseIfInvalidInitialValue() {
+    void parseHintMustReturnFalseIfInvalidInitialValue() {
         final Model model = new Model();
         final Variable x1 = model.addBinaryVariable(1.0);
         model.addEqualityConstraint(1.0, LinearExpression.of(new LinearExpression.Term(x1, 1.0)));
 
-        assertFalse(model.parseInitialSolution(InitialSolution.of(Map.of(x1, -1.0))));
+        assertFalse(model.parseHint(Hint.of(Map.of(x1, -1.0))));
         final Solution solution = model.maximize().orElseThrow();
         assertEquals(1.0, solution.getObjectiveValue(), EPSILON);
     }
 
     @Test
-    void parseInitialSolutionMustReturnFalseWhenInitialSolutionIsEmpty() {
+    void parseHintMustReturnFalseWhenHintIsEmpty() {
         final Model model = new Model();
         final Variable x1 = model.addBinaryVariable(1.0);
         model.addEqualityConstraint(1.0, LinearExpression.of(new LinearExpression.Term(x1, 1.0)));
 
-        assertFalse(model.parseInitialSolution(new InitialSolution()));
+        assertFalse(model.parseHint(new Hint()));
         final Solution solution = model.maximize().orElseThrow();
         assertEquals(1.0, solution.getObjectiveValue(), EPSILON);
     }
 
     @Test
-    void parseInitialSolutionMustReturnTrue() {
+    void parseHintMustReturnTrue() {
         final Model model = new Model();
         final Variable x1 = model.addBinaryVariable(1.0);
         final Variable x2 = model.addBinaryVariable(1.0);
@@ -59,7 +58,7 @@ class ModelWarmStartTest {
                 new LinearExpression.Term(x3, 1.0)
         ));
 
-        assertTrue(model.parseInitialSolution(InitialSolution.of(Map.of(x1, 1.0, x2, 0.0, x3, 0.0))));
+        assertTrue(model.parseHint(Hint.of(Map.of(x1, 1.0, x2, 0.0, x3, 0.0))));
         final Solution solution = model.maximize().orElseThrow();
         assertEquals(1.0, solution.getObjectiveValue(), EPSILON);
     }
