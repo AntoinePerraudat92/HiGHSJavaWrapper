af498bd886f01eab292e990c63cce62f94c6270c
antoine <antoine.perraudat@planimize.com>
2026-04-25 17:04:07 +0200

    updated comment

--- src/test/java/wrapper/util/ObjectCreator.java
+++ src/test/java/wrapper/util/ObjectCreator.java
@@ -6,11 +6,11 @@ import wrapper.model.option.BooleanOptions;
 public class ObjectCreator {
 
     /**
-     * This method is used to avoid corrupted channel error when working with native stream.
+     * This method is used to avoid corrupted channel error when working with native stream and maven.
      */
     public static Model createModel() {
         final Model model = new Model();
-        model.parseOption(BooleanOptions.SOLVER_OUTPUT.getOption(false));
+        model.parseOption(BooleanOptions.SOLVER_OUTPUT.getOption(true));
         return model;
     }
 
