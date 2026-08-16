8e8cbdcfb680f86534e88338c73dbb85f91baf9b
antoine <antoine.perraudat@planimize.com>
2026-08-16 10:33:46 +0200

    remove unnecessary echos

--- generate_shared_libraries_windows.bat
+++ generate_shared_libraries_windows.bat
@@ -2,16 +2,8 @@ setlocal
 
 copy "%HIGHS_HOME%\build\Release\bin\highs.dll" . || exit /b 1
 copy "%HIGHS_HOME%\build\Release\bin\highs.lib" . || exit /b 1
-
 cd src\main\java\highs || exit /b 1
-
-echo "COMPILING"
 cl /O2 /EHsc /I"%JAVA_HOME%\include" /I"%JAVA_HOME%\include\win32" /I"%HIGHS_HOME%\highs" /I"%HIGHS_HOME%\build" /c swig_java_highs_wrap.cxx || exit /b 1
-
-echo "dumpbin"
-dumpbin /linkermember ..\..\..\..\highs.lib | findstr checkInfo
-
-echo "LINKING"
 link /DLL /OUT:..\..\..\..\highswrap.dll swig_java_highs_wrap.obj /LIBPATH:..\..\..\.. highs.lib || exit /b 1
 
 endlocal
