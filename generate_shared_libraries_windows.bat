67ebf0118b43b5d67b81140d1d235ccf22350633
antoine <antoine.perraudat@planimize.com>
2026-08-16 09:30:56 +0200

    debug

--- generate_shared_libraries_windows.bat
+++ generate_shared_libraries_windows.bat
@@ -7,6 +7,6 @@ cd src\main\java\highs || exit /b 1
 
 cl /O2 /EHsc /I"%JAVA_HOME%\include" /I"%JAVA_HOME%\include\win32" /I"%HIGHS_HOME%\highs" /I"%HIGHS_HOME%\build" /c swig_java_highs_wrap.cxx || exit /b 1
 
-link /DLL /OUT:..\..\..\..\highswrap.dll swig_java_highs_wrap.obj highs.lib || exit /b 1
+link /DLL /OUT:..\..\..\..\highswrap.dll swig_java_highs_wrap.obj ..\..\..\..\highs.lib || exit /b 1
 
 endlocal
