6f4cc496a4e0d1a4f656e394ee978d1f60f9474c
antoine <antoine.perraudat@planimize.com>
2026-08-16 10:00:17 +0200

    debug

--- generate_shared_libraries_windows.bat
+++ generate_shared_libraries_windows.bat
@@ -9,6 +9,6 @@ echo "COMPILING"
 cl /O2 /EHsc /I"%JAVA_HOME%\include" /I"%JAVA_HOME%\include\win32" /I"%HIGHS_HOME%\highs" /I"%HIGHS_HOME%\build" /c swig_java_highs_wrap.cxx || exit /b 1
 
 echo "LINKING"
-link /DLL /OUT:..\..\..\..\highswrap.dll swig_java_highs_wrap.obj ..\..\..\..\highs.lib || exit /b 1
+link /DLL /OUT:..\..\..\..\highswrap.dll swig_java_highs_wrap.obj /LIBPATH:..\..\..\.. highs.lib || exit /b 1
 
 endlocal
